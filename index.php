fe8b8a5bc941f1cb7ccf4003a51cbd95f241aefd
alexandergolovan <golovan15@gmail.com>
2016-04-26 12:51:57 +0300

    Automatic pen

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 
 class Rod
 {
-    const COLOR_BLUE = "[034m";
-    const COLOR_RED = "[031m";
-    const COLOR_GREEN = "[032m";
-    const COLOR_GREY = "[030m";
+    const COLOR_BLUE   = "[034m";
+    const COLOR_RED    = "[031m";
+    const COLOR_GREEN  = "[032m";
+    const COLOR_GREY   = "[030m";
     const COLOR_YELLOW = "[033m";
-    const COLOR_WHITE = "[037m";
+    const COLOR_WHITE  = "[037m";
 
     public static $ERROR_ROD_EMPTY = "Opps rod is empty!";
 
@@ -77,6 +77,30 @@ class Pen {
     }
 }
 
+class AutomaticPen extends Pen {
+    public static $ERROR_PEN_CLOSE = 'Automatic pen is closed!';
+
+    private $isOpen = false;
+
+    public function __construct(Body $body, Rod $rod, $brand)
+    {
+        parent::__construct($body, $rod, $brand);
+    }
+
+    public function click () {
+        $this->isOpen = !$this->isOpen;
+    }
+
+    public function write($text)
+    {
+        if ($this->isOpen) {
+            parent::write($text);
+        } else {
+            echo " " . self::$ERROR_PEN_CLOSE . "\n";
+        }
+    }
+}
+
 class PenBuilder {
     public $brand = "Bic";
     public $bodyColor = "yellow";
@@ -85,9 +109,13 @@ class PenBuilder {
     public $bodyMaterial = 'plastic';
 
     public function build () {
-        $rod = new Rod($this->rodColor, $this->rodVolume);
-        $body = new Body($this->bodyColor, $this->bodyMaterial);
+        $rod = $this->createRod();
+        $body = $this->createBody();
 
+        return $this->createPen ($body, $rod);
+    }
+
+    protected function createPen ($body, $rod) {
         return new Pen ($body, $rod, $this->brand);
     }
 
@@ -99,6 +127,20 @@ class PenBuilder {
 
         return $this->build();
     }
+
+    protected function createBody () {
+        return new Body($this->bodyColor, $this->bodyMaterial);
+    }
+
+    protected function createRod () {
+        return new Rod($this->rodColor, $this->rodVolume);
+    }
+}
+
+class AutomaticPenBuilder extends PenBuilder {
+    protected function createPen ($body, $rod) {
+        return new AutomaticPen ($body, $rod, $this->brand);
+    }
 }
 
 
@@ -107,3 +149,14 @@ $penBuilder = new PenBuilder();
 $bicPenRed = $penBuilder->createBicPenRed();
 
 $bicPenRed->write('This is simple pen writing very well');
+
+$automaticPenBuilder = new AutomaticPenBuilder();
+$automaticBicPenRed = $automaticPenBuilder->createBicPenRed();
+
+$automaticBicPenRed->write('This is automatic is closed');
+$automaticBicPenRed->click();
+$automaticBicPenRed->write('This is automatic is opened');
+
+
+
+
